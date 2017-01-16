85b7bd9a0727b376a3f27220228c06f3cfdaea7e
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-01-16 21:36:48 +0000

    Added comments and slightly reorganized code for clarity.

--- download_avatars.js
+++ download_avatars.js
@@ -1,11 +1,10 @@
 var request = require('request');
 var fs = require('fs');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
-
 var GITHUB_USER = "amccrodan";
 var GITHUB_TOKEN = "581cc3110dadb29e6abeec829e6beb65f5b422a4";
 
+// Requests a JSON-formatted array of contributors to a certain repo on GitHub
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url : 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
@@ -21,6 +20,7 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
+// Downloads an image from a specific URL to the given filePath
 function downloadImageByURL(url, filePath) {
 
   request.get(url)
@@ -30,19 +30,29 @@ function downloadImageByURL(url, filePath) {
    .pipe(fs.createWriteStream(filePath));
 }
 
+
+
+// Welcome!
+console.log('Welcome to the GitHub Avatar Downloader!');
+
+// Take in command line arguments, ignoring the location of Node and this file
 var myArgs = process.argv.slice(2);
 
+// Check to make sure two arguments have been passed in
 if (myArgs.length !== 2) {
   console.log("Please enter exactly 2 arguments, in the form <owner> <repo>.");
   return;
 }
 
+// Call the main function with command line input
 getRepoContributors(myArgs[0], myArgs[1], function(err, result) {
 
+  // Check to see if avatars directory already exists or not
   if (!fs.existsSync('./avatars')) {
     fs.mkdirSync('./avatars');
   }
 
+  // Loop through each contributor and download their avatar to local folder
   for (var i = 0; i < result.length; i++) {
     var filePath = 'avatars/' + result[i].login + '.jpg';
     downloadImageByURL(result[i].avatar_url, filePath);
