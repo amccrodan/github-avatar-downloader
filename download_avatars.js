736b9b2bd697403adeba68eae3784b7381c28d1d
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-01-16 20:03:44 +0000

    Tested that getRepoContributors built a valid URL from inputs.

--- download_avatars.js
+++ download_avatars.js
@@ -2,8 +2,12 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+var GITHUB_USER = "amccrodan";
+var GITHUB_TOKEN = "581cc3110dadb29e6abeec829e6beb65f5b422a4";
+
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
 }
 
 
