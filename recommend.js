edf175c673b9be8742191afadab9a2cfca18d72c
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-01-17 02:54:38 +0000

    Updated to output starred repos of first contributor to lighthouse-labs/laser_shark.

--- recommend.js
+++ recommend.js
@@ -1,4 +1,29 @@
+require('dotenv').config();
 var get_contrib = require('./get-contributors');
+var request = require('request');
+
+var GITHUB_USER = process.env.GITHUB_USER;
+var GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+
+
+function getStarredRepos (url) {
+  var options = {
+    url: url, //'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/users/kvirani/starred',
+    headers: {
+      'User-Agent': "GitHub Repo Recommender - Student Project"
+    },
+    json: true
+  };
+
+  var starredRepos = [];
+  request.get(options, function(err, response, body) {
+    //console.log(result[0].full_name);
+    for (var i = 0; i < body.length; i++) {
+    console.log(body[i].full_name);
+    }
+  })
+  .auth(null, null, true, GITHUB_TOKEN);
+}
 
 // Take in command line arguments, ignoring the location of Node and this file
 var myArgs = process.argv.slice(2);
@@ -11,7 +36,8 @@ if (myArgs.length !== 2) {
 
 // Call the main function with command line input
 get_contrib.getRepoContributors(myArgs[0], myArgs[1], function(err, result) {
-  for (var i = 0; i < result.length; i++) {
-    console.log(result[i].starred_url);
-  }
+  getStarredRepos(result[0].starred_url.replace('{/owner}{/repo}', ''));
+  // for (var i = 0; i < result.length; i++) {
+  //   getStarredRepos(result[i].starred_url.replace('{/owner}{/repo}', ''));
+  // }
 });
