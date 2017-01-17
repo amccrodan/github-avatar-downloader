2e65d04c0ae2b0b17456304c06bb2f9d39aec67d
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-01-17 01:14:40 +0000

    Updated to require new get-contributors module.

--- recommend.js
+++ recommend.js
@@ -1,4 +1,4 @@
-
+var get_contrib = require('./get-contributors');
 
 // Take in command line arguments, ignoring the location of Node and this file
 var myArgs = process.argv.slice(2);
@@ -9,30 +9,7 @@ if (myArgs.length !== 2) {
   process.exit();
 }
 
-
-// ENV file validation
-if (!fs.existsSync('./.env')) {
-  console.log('The .env file containing user information does not exist.');
-  process.exit();
-}
-
-if (!GITHUB_USER || !GITHUB_TOKEN) {
-  console.log('Missing username or token in the .env file.');
-  process.exit();
-}
-
-
 // Call the main function with command line input
-getRepoContributors(myArgs[0], myArgs[1], function(err, result) {
-  // Check to see if avatars directory already exists or not
-  if (!fs.existsSync('./avatars')) {
-    fs.mkdirSync('./avatars');
-  }
-
-  // Loop through each contributor and download their avatar to local folder
-  for (var i = 0; i < result.length; i++) {
-    var filePath = 'avatars/' + result[i].login + '.jpg';
-    downloadImageByURL(result[i].avatar_url, filePath);
-  }
-
+get_contrib.getRepoContributors(myArgs[0], myArgs[1], function(err, result) {
+  console.log(result);
 });
