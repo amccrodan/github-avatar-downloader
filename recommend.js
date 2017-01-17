60b034d4a8e752782386e5197a16f881eb4c00a4
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-01-17 03:45:42 +0000

    Succeeded in returning all starred repo full_names. Updated to count them into an object.

--- recommend.js
+++ recommend.js
@@ -6,7 +6,7 @@ var GITHUB_USER = process.env.GITHUB_USER;
 var GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 
 
-function getStarredRepos (url) {
+function getStarredRepos (url, tallyObject) {
   var options = {
     url: url, //'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/users/kvirani/starred',
     headers: {
@@ -17,9 +17,9 @@ function getStarredRepos (url) {
 
   var starredRepos = [];
   request.get(options, function(err, response, body) {
-    //console.log(result[0].full_name);
+    //console.log(response.headers);
     for (var i = 0; i < body.length; i++) {
-    console.log(body[i].full_name);
+    tallyObject[body[i].full_name] = (!tallyObject[body[i].full_name]) ? 1 : tallyObject[body[i].full_name] + 1;
     }
   })
   .auth(null, null, true, GITHUB_TOKEN);
@@ -36,8 +36,11 @@ if (myArgs.length !== 2) {
 
 // Call the main function with command line input
 get_contrib.getRepoContributors(myArgs[0], myArgs[1], function(err, result) {
-  getStarredRepos(result[0].starred_url.replace('{/owner}{/repo}', ''));
-  // for (var i = 0; i < result.length; i++) {
-  //   getStarredRepos(result[i].starred_url.replace('{/owner}{/repo}', ''));
-  // }
+
+  var tallyObject = {};
+
+  for (var i = 0; i < result.length; i++) {
+    getStarredRepos(result[i].starred_url.replace('{/owner}{/repo}', ''), tallyObject);
+  }
+
 });
